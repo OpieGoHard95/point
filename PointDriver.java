207b91c1f81177d28a3bc04f37aa9916f3902f13
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-01-30 15:43:30 -0500

    Added comments to the PointDriver :)

--- PointDriver.java
+++ PointDriver.java
@@ -1,10 +1,16 @@
+// PointDriver for CMP112
+// Jared Smith :)
+// 30 Jan 15
+
 public class PointDriver {
 
+// Make three ponts
 	public static void main(String[] args ) {
 	Point lowerLeft = new Point();
 	Point upperRight = new Point();
 	Point middlePoint = new Point();
 
+// Give them some values
 	lowerLeft.x = 0.0;
 	lowerLeft.y = 0.0;
 
@@ -14,8 +20,11 @@ public class PointDriver {
 	middlePoint.x = 640.0;
 	middlePoint.y = 512.0;
 
+//Print out one of the points
 	upperRight.printPoint();
+//Clear the point
 	upperRight.clear();
+// Print out the new point
 	upperRight.printPoint();
 	}
 }
